ce7f65e3520b9b3bd0f7ef5bd49f077ba29b392a
ODAWARA Yoshito <odawarayoshito@icloud.com>
2025-01-18 06:39:22 +0800

    Update TestDataSeeder.php

--- database/seeders/TestDataSeeder.php
+++ database/seeders/TestDataSeeder.php
@@ -10,11 +10,18 @@ class TestDataSeeder extends Seeder
 {
     public function run()
     {
-        // テストデータを一括登録
-        // 会社100件を作成
-        Company::factory(100)->create();
+        // 対象ユーザーIDを指定
+        $userId = 5;
 
-        // 人物100件を作成
-        Person::factory(100)->create();
+        // 会社100件を作成
+        Company::factory(100)->create([
+            'user_id' => $userId, // ユーザーIDを指定
+        ])->each(function ($company) use ($userId) {
+            // 各会社に関連する人物を作成
+            Person::factory(1)->create([
+                'company_id' => $company->id,
+                'user_id' => $userId, // ユーザーIDを指定
+            ]);
+        });
     }
 }
